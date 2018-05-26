916aefd2b2507ff6517306132c9db1418f54b174
Chenshu <cyi@thoughtworks.com>
2018-05-26 08:54:35 +0800

    MISC - update webpack config - update vscode setting - install resolve url loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -87,6 +87,7 @@ module.exports = {
       
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      'components': path.resolve('src/components'),
       'react-native': 'react-native-web',
     },
     plugins: [
@@ -197,6 +198,9 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                 },
+              },
+              {
+                loader: require.resolve('resolve-url-loader'),
               },
               {
                 loader: require.resolve('sass-loader'),
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -93,6 +93,7 @@ module.exports = {
       
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      'components': path.resolve('src/components'),
       'react-native': 'react-native-web',
     },
     plugins: [
@@ -231,6 +232,9 @@ module.exports = {
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
+                    },
+                    {
+                      loader: require.resolve('resolve-url-loader'),
                     },
                     {
                       loader: require.resolve('sass-loader'),
