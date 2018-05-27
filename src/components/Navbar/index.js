3e8ab0b8e1021065a9654a7e6b554f7ccffe0c87
Chenshu <cyi@thoughtworks.com>
2018-05-27 15:11:02 +0800

    added tooltip and toast

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ export const Navbar = (props) => {
     <div className="navbar">
       <span className="navbar__back">&lt;</span>
       {props.children}
-      <div className="navbar__icon payment"></div>
+      <span className="navbar__icon payment" />
     </div>
   )
 }
--- src/components/Wallet/Header/index.js
+++ src/components/Wallet/Header/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { Navbar } from 'components/Navbar'
+import { Navbar } from 'components/Navbar';
+import { Toast } from 'components/Toast';
+import { Tooltip } from 'components/Tooltip';
+
 import './header.scss';
 
 export class Header extends Component {
+  state = {
+    showToast: true,
+    showTip: false,
+  }
+
+  dismissToast = () => {
+    this.setState({showToast: false, showTip: true});
+  }
   render() {
+    const { showTip, showToast } = this.state;
     return (
       <div className="wallet-header">
         <Navbar>
@@ -11,6 +23,19 @@ export class Header extends Component {
             GrabPay<br /><span className="subtitle">by OVO</span>
           </div>
         </Navbar>
+        <div className="tooltip-container">
+        {showTip &&
+          <Tooltip>
+            <p>Check your payment methods here.</p>
+          </Tooltip>}
+        </div>
+        {showToast && 
+          <Toast dismiss={this.dismissToast}>
+            <span className="wallet-activated-icon"/>
+            <p className="wallet-activated-text">
+              All set! You can start using your balance now.
+            </p>
+          </Toast>}
         <div className="balance">
           <span className="currency">idr</span>
           <span className="value">50.000</span>
